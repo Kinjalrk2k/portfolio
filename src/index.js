b9fb3b64ea47b5ee61931b86faaafbfcdba197b6
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-03-12 21:49:49 +0530

    added chakra ui and dark theme

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
+import { ChakraProvider } from "@chakra-ui/react";
+
 import App from "./components/App";
 import reducers from "./reducers";
+import theme from "./themes/dark";
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </Provider>,
   document.querySelector("#root")
 );
