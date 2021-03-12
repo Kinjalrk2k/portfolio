ee0cfd5c664323a7a39a7f10c6fb821168d990e3
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-03-12 21:00:47 +0530

    added static json data for now

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,11 @@
-import { db } from "../services/firebase";
+// import { db } from "../services/firebase";
+import data from "../data/data.json";
 
 export const fetchData = () => async (dispatch) => {
-  const data = await db
-    .ref("/")
-    .once("value")
-    .then((snapshot) => snapshot.val());
+  // const data = await db
+  //   .ref("/")
+  //   .once("value")
+  //   .then((snapshot) => snapshot.val());
 
   dispatch({
     type: "FETCH_DATA",
