7f1ac174292f8f673e666b990505e8403e0acd0d
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-03-27 11:17:38 +0530

    data fetching from firebase

--- src/actions/data.js
+++ src/actions/data.js
@@ -1,16 +1,21 @@
-// import { db } from "../services/firebase";
+import { db } from "../services/firebase";
 import data from "../data/data.json";
 
 export const fetchData = () => async (dispatch) => {
-  // const data = await db
-  //   .ref("/")
-  //   .once("value")
-  //   .then((snapshot) => snapshot.val());
+  const data = await db
+    .ref("/")
+    .once("value")
+    .then((snapshot) => snapshot.val());
 
-  setTimeout(() => {
-    dispatch({
-      type: "FETCH_DATA",
-      payload: data,
-    });
-  }, 1000);
+  dispatch({
+    type: "FETCH_DATA",
+    payload: data,
+  });
+
+  // setTimeout(() => {
+  //   dispatch({
+  //     type: "FETCH_DATA",
+  //     payload: data,
+  //   });
+  // }, 1000);
 };
