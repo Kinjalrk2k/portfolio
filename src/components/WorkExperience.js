72435f02ee29ff31eeea27c416dcc30d8621d684
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-04-05 00:03:10 +0530

    completed basic work xp section

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import CommentedHeading from "./partials/CommentedHeading";
 import {
   VerticalTimeline,
@@ -20,46 +21,84 @@ import {
 } from "@chakra-ui/react";
 
 import { AiFillApi } from "react-icons/ai";
-import { MdCheckCircle } from "react-icons/md";
+import { MdCheckCircle, MdWork } from "react-icons/md";
 
-const WorkExperience = (props) => {
-  return (
-    <div>
-      <CommentedHeading body="Work Experience" />
-      <VerticalTimeline animate={false}>
+class WorkExperience extends React.Component {
+  onGoingBadge(work) {
+    if (work.isOngoing) {
+      return (
+        <Badge fontSize="0.7em" ml={2} colorScheme="purple">
+          Ongoing
+        </Badge>
+      );
+    }
+    return null;
+  }
+
+  renderDetailsList(details) {
+    return details.map((detail) => {
+      return (
+        <ListItem>
+          <ListIcon as={MdCheckCircle} color="green.500" />
+          {detail}
+        </ListItem>
+      );
+    });
+  }
+
+  renderPositionList(work) {
+    return work.positions.map((pos) => {
+      return (
+        <div>
+          <Text fontSize="xl" color="#718096">
+            {pos.designation}
+          </Text>
+          <List spacing={1} fontSize={14}>
+            {this.renderDetailsList(pos.details)}
+          </List>
+        </div>
+      );
+    });
+  }
+
+  renderWorkList() {
+    return this.props.workExperience.map((work) => {
+      return (
         <VerticalTimelineElement
           className="vertical-timeline-element"
           contentArrowStyle={{ borderRight: "7px solid  #4A5568" }}
           contentStyle={{ backgroundColor: "#171923" }}
           iconStyle={{ background: "#171923", color: "#fff" }}
-          icon={<AiFillApi />}
+          icon={<MdWork />}
         >
           <div style={{ marginBottom: "15px" }}>
             <Heading as="h3" size="lg">
-              UNIGO Cabs{" "}
-              <Badge fontSize="0.7em" colorScheme="purple">
-                Ongoing
-              </Badge>
+              {work.organization}
+              {this.onGoingBadge(work)}
             </Heading>
-            <Text fontSize="xl" color="#718096">
-              Backend Developer
-            </Text>
           </div>
 
-          <List spacing={1} style={{ marginBottom: "15px" }}>
-            <ListItem>
-              <ListIcon as={MdCheckCircle} color="green.500" />
-              Developing Backend APIs for Android app
-            </ListItem>
-            <ListItem>
-              <ListIcon as={MdCheckCircle} color="green.500" />
-              Managing and maintaining Databases
-            </ListItem>
-          </List>
+          {this.renderPositionList(work)}
         </VerticalTimelineElement>
-      </VerticalTimeline>
-    </div>
-  );
+      );
+    });
+  }
+
+  render() {
+    console.log(this.props.workExperience);
+    return (
+      <div>
+        <CommentedHeading body="Work Experience" />
+        <VerticalTimeline animate={false}>
+          {this.renderWorkList()}
+        </VerticalTimeline>
+      </div>
+    );
+  }
+}
+
+const mapStateToprops = (state) => {
+  return { workExperience: state.data.workExperience };
 };
 
-export default WorkExperience;
+export default connect(mapStateToprops)(WorkExperience);
